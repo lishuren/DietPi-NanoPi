1352c074d1f505c560457cbf5214d7f2b3ac58f2
Gimbal Lock <9226566@qq.com>
2025-12-23 12:58:12 +0800

    fix download url

--- scripts/download_image.sh
+++ scripts/download_image.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# URL for DietPi NanoPi NEO image (ARMv7)
+# URL for DietPi NanoPi NEO2 image (ARM64)
 # Note: Links can change, checking DietPi website is recommended.
-# This is the direct link for NanoPi NEO (Bullseye/Bookworm depending on release)
-IMAGE_URL="https://dietpi.com/downloads/images/DietPi_NanoPiNEO-ARMv7-Bookworm.7z"
-OUTPUT_FILE="DietPi_NanoPiNEO.7z"
+# This is the direct link for NanoPi NEO2 Bookworm (stable)
+IMAGE_URL="https://dietpi.com/downloads/images/DietPi_NanoPiNEO2-ARMv8-Bookworm.img.xz"
+OUTPUT_FILE="DietPi_NanoPiNEO2-ARMv8-Bookworm.img.xz"
 
-echo "Downloading DietPi image for NanoPi NEO..."
+echo "Downloading DietPi image for NanoPi NEO2 (Bookworm, ~200MB)..."
 curl -L -o "$OUTPUT_FILE" "$IMAGE_URL"
 
 if [ $? -eq 0 ]; then
