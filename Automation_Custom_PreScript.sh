32ab347a2df7ea24471fd73c1f5e5903ac06ae0e
Gimbal Lock <9226566@qq.com>
2025-12-29 13:14:42 +0800

    CRLF to LF

--- Automation_Custom_PreScript.sh
+++ Automation_Custom_PreScript.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Manually map GitHub domains to known IP addresses
 echo "20.205.243.166  github.com" >> /etc/hosts
-echo "185.199.111.133 raw.githubusercontent.com" >> /etc/hosts
+echo "185.199.109.133 raw.githubusercontent.com" >> /etc/hosts
